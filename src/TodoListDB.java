777c3ae597cb25ce008043d539d58e354457b8f1
issuser <issuser@ISS-CR-31220.stf.nus.edu.sg>
2019-04-10 14:36:16 +0800

    11

---
+++ src/TodoListDB.java
@@ -0,0 +1,46 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
+import java.util.HashMap;
+public class TodoListDB {
+	
+	private HashMap<Integer, TodoList> mDB = new HashMap<Integer,TodoList>();
+	
+	
+	public void Add(TodoList item)
+	{
+		mDB.put(item.getID(),item);
+	}
+
+	public boolean IsInList(Integer id)
+	{
+		if (mDB.containsKey(id))
+			return true;
+		return false;
+	}
+	
+	public boolean Remove(Integer id)
+	{
+		if (IsInList(id))
+		{
+			mDB.remove(id);
+			return true;
+		}
+		return false;
+	}
+	
+	public TodoList Get(Integer id)
+	{
+		return (TodoList)mDB.get(id);
+	}
+	
+	public void testDate()
+	{
+		DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		//DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		Date tmpDate = new Date();
+		System.out.println(sdf.format( tmpDate));
+	}
+}
